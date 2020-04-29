806fa6d3647132fcd28ac36aa07919edcb4fa98b
Johniel Bocacao <50508869+johnielb@users.noreply.github.com>
2020-04-29 14:32:07 +1200

    Implemented 3-way contingency tables, changed formatting console messages, FET more efficient

--- 02-contingency.R
+++ 02-contingency.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 z_from_confidence_level <- function(confidence.level) {
   significance_level <- 1-confidence.level
   qnorm(1-significance_level/2)
@@ -17,7 +19,7 @@ odds_ratio <- function(n11, n12, n21, n22, z=NA, confidence.level=NA) {
   }
   se <- z * sqrt(1/n11+1/n12+1/n21+1/n22)
   interval <- c(exp(log(theta_hat) - se), exp(log(theta_hat) + se))
-  cat(paste0(confidence.level*100, "% Confidence interval for odds ratio: (", interval[1], ", ", interval[2], ")\n"))
+  cat(paste0("Odds ratio estimate = ", theta_hat, "\n", confidence.level*100, "% confidence interval for odds ratio: (", interval[1], ", ", interval[2], ")\n\n"))
 }
 
 expected_fs <- function(tbl, check=F) {
@@ -31,11 +33,11 @@ expected_fs <- function(tbl, check=F) {
       f_hat[i,j] <- row_marginals[[i]]*col_marginals[[j]]/total;
       if (f_hat[i,j] < 5) {
         found <- T
-        if (check) cat(paste0("n",i,j," = ",tbl[[i]][[j]], " has an expected frequency of ", f_hat, ", smaller than 5.\n"))
+        if (check) cat(paste0("n",i,j," = ",tbl[[i]][[j]], " has an expected frequency of ", f_hat, ", smaller than 5.\n\n"))
       }
     }
   }
-  if (check && !found) cat("All expected frequencies are at least 5.\n")
+  if (check && !found) cat("All expected frequencies are at least 5.\n\n")
   if (!check) f_hat
 }
 
@@ -47,12 +49,18 @@ chisq_independence_test <- function(tbl, pearson=T, likelihood=T) {
   p_value_chi <- pchisq(sum(chisq), df=df, lower.tail=F)
   p_value_g <- pchisq(sum(like_ratio), df=df, lower.tail=F)
   if (pearson) {
-    cat(paste0("Pearson chi-square test: X^2 = ",sum(chisq),"\nP(chisq_",df," > X^2) = ",p_value_chi,"\n"));
+    cat(paste0("Pearson chi-square test: X^2 = ",sum(chisq),"\nP(chisq_",df," > X^2) = ",p_value_chi,"\n\n"));
   }
   if (likelihood) {
-    cat(paste0("Likelihood ratio chi-square test: G^2 = ",sum(like_ratio),"\nP(chisq_",df," > G^2) = ",p_value_g),"\n");
+    cat(paste0("Likelihood ratio chi-square test: G^2 = ",sum(like_ratio),"\nP(chisq_",df," > G^2) = ",p_value_g),"\n\n");
   }
 }
+
+cancer <- matrix(c(13,3,2,12), byrow=T, nrow=2)
+colnames(cancer) <- c("Survived 10 years+", "Survived less than 10 years")
+odds_ratio(cancer[1,1], cancer[1,2], cancer[2,1], cancer[2,2], confidence.level = 0.95)
+expected_fs(cancer, check=T)
+chisq_independence_test(cancer)
 
 fishers_exact_test <- function(tbl, sig.level=0.05, lower.tail=F, two.sided=F) {
   row_marginals <- apply(tbl, 1, sum)
@@ -61,32 +69,63 @@ fishers_exact_test <- function(tbl, sig.level=0.05, lower.tail=F, two.sided=F) {
   n <- row_marginals[[2]]
   k <- apply(tbl, 2, sum)[[1]]
   table_p_val <- dhyper(x,m,n,k)
-  right_p_val <- phyper(x-1,m,n,k, lower.tail=F)
-  left_p_val <- phyper(x,m,n,k)
-  if (two.sided) {
+  if (two.sided) { # get all p-values as extreme as the table p value
     p_vals <- dhyper(0:min(m,k), m, n, k)
-    two_sided_p_val <- sum(p_vals[p_vals<=table_p_val])
+    p_val <- sum(p_vals[p_vals<=table_p_val])
     mid_p_val <- two_sided_p_val - table_p_val
-    cat(paste0("\np-value of test: ", two_sided_p_val))
   } else {
     if (lower.tail) {
-      mid_p_val <- left_p_val - 0.5*table_p_val
-      cat(paste0("\np-value of test: ", left_p_val))
+      p_val <- phyper(x-1,m,n,k, lower.tail=F)
     } else {
-      mid_p_val <- right_p_val - 0.5*table_p_val
-      cat(paste0("\np-value of test: ", right_p_val))
+      p_val <- phyper(x,m,n,k)
     }
+    mid_p_val <- p_val - 0.5*table_p_val
   }
-  result <- "\n> Can't reject null hypothesis, insufficient evidence to suggest the two proportions are different.\n"
+  cat(paste0("p-value of test: ", p_val, "\nmid p-value of test: ", mid_p_val))
   if (mid_p_val < sig.level) {
-    result <- "\n> Can reject null hypothesis, sufficient evidence to suggest the two proportions are different.\n"
+    cat("\n> Can reject null hypothesis, sufficient evidence to suggest the two proportions are different.\n\n")
+  } else {
+    cat("\n> Can't reject null hypothesis, insufficient evidence to suggest the two proportions are different.\n\n")
   }
-  cat(paste0("\nmid p-value of test: ", mid_p_val, result))
 }
 
-tbl <- matrix(c(13,3,2,12), byrow=T, nrow=2)
-colnames(tbl) <- c("Survived 10 years+", "Survived less than 10 years")
-expected_fs(tbl, check=T)
-chisq_independence_test(tbl)
 fishers_exact_test(tbl)
 fishers_exact_test(tbl, two.sided=T)
+
+conditional_associations <- function(tbls) {
+  for (i in seq_along(tbls)) {
+    cat(paste0("Z = ", names(tbls)[[i]], ": "))
+    m <- tbls[[i]]
+    odds_ratio(m[1,1], m[1,2], m[2,1], m[2,2], confidence.level = 0.95)
+  }
+}
+
+# construct 3 way contingency table
+death_penalty <- list(matrix(c(19,132,11,52), byrow=T, nrow=2),
+             matrix(c(0,9,6,97), byrow=T, nrow=2)) %>% 
+  lapply(function(m) {
+    rownames(m) <- c("Cauc defendant", "AA defendant")
+    colnames(m) <- c("No death penalty", "Death penalty")
+    m
+  })
+names(death_penalty) <- c("African-American defendant", "Caucasian defendant")
+
+marginal_tbl <- Reduce('+', death_penalty)
+odds_ratio(marginal_tbl[1,1], marginal_tbl[1,2], marginal_tbl[2,1], marginal_tbl[2,2], confidence.level = 0.95)
+
+conditional_associations(death_penalty)
+
+
+graduates <- list(matrix(c(18,12,12,8), byrow=T, nrow=2),
+                  matrix(c(2,8,8,32), byrow=T, nrow=2)) %>% 
+  lapply(function(m) {
+    rownames(m) <- c("Female", "Male")
+    colnames(m) <- c("Low income", "High income")
+    m
+  })
+names(death_penalty) <- c("Arts", "Science")
+
+marginal_tbl <- Reduce('+', graduates)
+odds_ratio(marginal_tbl[1,1], marginal_tbl[1,2], marginal_tbl[2,1], marginal_tbl[2,2], confidence.level = 0.95)
+
+conditional_associations(graduates)
